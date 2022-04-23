21182a6774c0317aaca4f0de395c062b49d2509c
Darling <69660798+d4rl@users.noreply.github.com>
2022-04-23 14:35:43 -0300

    Solved a lot of errors.

--- server/main.py
+++ server/main.py
@@ -1,91 +1,70 @@
-import aiohttp
+import json
+import ssl, msgpack, flask
+from flask import Flask, request, jsonify
 from json import dumps, loads
 from sys import exit
-from aiohttp import web
 from handler.persist_data import Agents
 from menus.utils import *
 
+# Create database and flask instance
 agentsDatabase = Agents()
+app = Flask(__name__)
 
-class C2Server:
-	async def agentList(request):
-		agents = agentsDatabase.loadAgents()
+@app.route("/agent/list")
+def agentList():
+	agents = agentsDatabase.loadAgents()
 
-		return web.json_response(data = agents)
+	if agents[0] != 0:
+		return jsonify(
+			sucess=1,
+			error="{agents[1]}" 
+		)
 
-	async def webSocket(request):
-		Print.good("Starting websocket")
+	return jsonify({
+		"sucess" : 0,
+		"agents" : f"{agents[1]}"
+	})
 
-		# Open websocket
-		ws = web.WebSocketResponse()
+@app.route("/agent/add", methods = ["POST"])
+def addAgent():
+	
+	listening_ip = request.args.post('listening_ip')
+	listening_pt = request.args.post('listening_pt')
+	name = request.args.post('name')	
+	
+	# Check for none params
+	if listening_pt == None or listening_pt == None or name == None:	
+		jsonify({
+			"sucess"  : 1,
+			"error"   : "Cannot add an empty agent."
+		})
 
-		# Prepare websocket for connections
-		await ws.prepare()
+	# Create an instance for errors handling
+	agent = agentsDatabase.addAgent(data["listening_ip", "listening_pt"], data["name"])
 
-		# Listen for messages
-		async for msg in ws:
-			if msg.type == aiohttp.WSMsgType.TEXT:
-				# Response for command execution
-				if msg.data:
-					await ws.close()
-			
-			# Close connection
-			elif msg.type == aiohttp.WSMsgType.ERROR:
-				Print.bad(f"WSError: {ws.exception()}")
-
-	async def addAgent(request):
-		data  = await request.json()
+	# Check if error in database
+	if agent[0] != 0:
+		Print.bad(f"Can\'t add agent, error: {agent[1]}")
 		
-		# Just an basic check
-		try:
-			listening_ip = data['listening_ip']
-			listening_pt = data['listening_pt']
-			name 		 = data['name']
+		return jsonify({
+			"sucess"  : 1,
+			"error"   : f"{agent[1]}"
+		})
 
-		except:
-			return web.json_response(data = {
-				"sucess"  : 1,
-				"error"   : "Cannot add an empty agent."
-			})
-
-		# Create an instance for errors handling
-		agent = agentsDatabase.addAgent(data["listening_ip", "listening_pt"], data["name"])
-
-		if agent[0] != 0:
-			Print.bad(f"Can\'t add agent, error: {agent[1]}")
-			
-			return web.json_response(data = {
-				"sucess"  : 1,
-				"error"   : agent[1]
-			})
-
-		return web.json_response(data = {
-				"sucess"  : 0,
-			})
-		
+	return jsonify({
+		"sucess"  : 0,
+	})
+	
 def main():
 	Print.good("Initializing the database")
-	
-	# Initialize the database and create an instance
-	app = web.Application()
 	
 	# Check for database errors
 	if agentsDatabase.createAgentsDatabase()[0] != 0:
 		Print.bad(f"Cant create the database, error: {agentsDatabase.createAgentsDatabase()[1]}")
 		exit()
 
-	# Add routes to app
-	Print.good("Initializing routes")
-	app.add_routes([
-		web.get('/agents', C2Server.agentList),
-		web.get('/communicate', C2Server.webSocket),
-		
-		web.post('/agent/add', C2Server.addAgent),
-	])
-
-	# Run the app
-	Print.good("Routes ok")
-	web.run_app(app)
+	# Start flask server
+	app.run(debug=True, port=1337)
 
 
 if __name__ == '__main__':
